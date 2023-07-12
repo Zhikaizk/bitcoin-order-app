cf9c810eb8ba191eaf578ca79a45c70cc37e784e
Zhikaizk <zhikai.leong@ncs.com.sg>
2023-07-12 15:04:43 +0800

    lint, dast,sast3

--- scripts/zap-script.sh
+++ scripts/zap-script.sh
@@ -2,7 +2,6 @@
 
 docker pull owasp/zap2docker-stable
 docker run -i owasp/zap2docker-stable zap-baseline.py -t 
-"https://kenken64.github.io/bitcoin-order-app/" -l PASS > 
-zap_baseline_report.html
+"https://kenken64.github.io/bitcoin-order-app/" -l PASS > zap_baseline_report.html
 
 echo $? > /dev/null
